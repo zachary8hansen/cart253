c37524d6eb07bd9c5571dd153ca22dc09746d192
zachary8hansen <55223988+zachary8hansen@users.noreply.github.com>
2019-09-19 12:00:15 -0400

    Add files via upload
    added a moving square

--- Excercises/exercise1/js/script.js
+++ Excercises/exercise1/js/script.js
@@ -31,6 +31,11 @@ let squareY;
 
 let squareSize = 350;
 
+//Preparing the square that follows the mouse
+let square1X;
+
+let square1Y;
+
 function preload() {
 
 }
@@ -41,7 +46,7 @@ function setup() {
   // Create our canvas
 
   createCanvas(640,640);
-background(0,0,0);
+background(0,210,89);
 
 
   // Start the circles off screen to the bottom left
@@ -52,9 +57,9 @@ background(0,0,0);
 
   circleY = height + circleSize/2;
 
-  circle1X = -circleSize/0.5;
+  circle1X = -circle1Size/0.5;
 
-  circle1Y = height + circleSize/3;
+  circle1Y = height + circle1Size/3;
 
   // Start the square off screen to the bottom right
 
@@ -63,6 +68,7 @@ background(0,0,0);
   squareX = width + squareSize/2;
 
   squareY = height + squareSize/2;
+//
 
   // We'll draw rectangles from the center
 
@@ -84,6 +90,7 @@ function draw() {
   circle1X += 1;
 
   circle1Y -= 1;
+
   // Make the circle transparent red
 
   fill(255,0,0,10);
@@ -91,6 +98,8 @@ function draw() {
   // Display the circle
 
   ellipse(circleX,circleY,circleSize,circleSize);
+
+  //moving the 2nd circle
   ellipse(circle1X,circle1Y,circle1Size,circle1Size)
 
 
@@ -107,5 +116,13 @@ function draw() {
   // Display the square
 
   rect(squareX,squareY,squareSize,squareSize);
+
+//display the movement of square1
+  square1X = mouseX;
+  square1Y = mouseY;
+
+  fill(0,50,0,40)
+//display square1
+rect(square1X,square1Y,50,50)
 
 }
