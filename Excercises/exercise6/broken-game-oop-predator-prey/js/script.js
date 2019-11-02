0b0c60c3b9eb97ecefb67d4387c43890e1210d5f
zachary8hansen <55223988+zachary8hansen@users.noreply.github.com>
2019-11-01 20:30:13 -0400

    Add files via upload
    Added the fixed version of exercise 6. Most of it was numerical and grammatic errors.
    I would like to note that due to my dyslexia, I found this exercise extremely enraging.

---
+++ Excercises/exercise6/broken-game-oop-predator-prey/js/script.js
@@ -0,0 +1,58 @@
+// Predator-Prey Simulation
+// Zachary Hansen
+//
+// Creates a predator and three prey (of different sizes and speeds)
+// The predator chases the prey using the arrow keys and consumes them.
+// The predator loses health over time, so must keep eating to survive.
+
+////////////////////
+// 10 ERRORS IN HERE
+////////////////////
+
+// Our predator
+let tiger;
+
+// The three prey
+let antelope;
+let zebra;
+let bee;
+
+// setup()
+//
+// Sets up a canvas
+// Creates objects for the predator and three prey
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  tiger = new Predator(500, 300, 5, color(200, 200, 0), 40);
+  antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
+  zebra = new Prey(100, 100, 8, color(255, 255, 255), 60);
+  bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
+}
+
+// draw()
+//
+// Handles input, movement, eating, and displaying for the system's objects
+function draw() {
+  // Clear the background to black
+  background(0);
+
+// Handle input for the tiger
+tiger.handleInput();
+
+  // Move all the "animals"
+  tiger.move();
+  antelope.move();
+  zebra.move();
+  bee.move();
+
+  // Handle the tiger eating any of the prey
+  tiger.handleEating(antelope);
+  tiger.handleEating(zebra);
+  tiger.handleEating(bee);
+
+  // Display all the "animals"
+  tiger.display();
+  antelope.display();
+  zebra.display();
+  bee.display();
+}
